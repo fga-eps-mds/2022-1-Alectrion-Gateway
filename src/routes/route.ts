3804aadd57190e67374482ac5a1aa6d2bbd69a17
wildemberg-sales <junior_sales2010@hotmail.com>
2022-07-30 00:24:25 -0300

    #91 subindo versão com portas funcional, mas não restrita

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,41 +1,18 @@
 import express, { json } from 'express'
-import cors from 'cors'
-import httpProxy from 'express-http-proxy'
-
-const proxy = require('express-http-proxy')
 
 const app = express()
 
-const port = 4001
-
-/*
 app.use(json())
 
-app.use(cors())
-app.use('/users', proxy('http:localhost:4001'))
-app.listen(port, () => console.log(`testando porta ${port}`))
-*/
-
-function selectProxyHost(req){
-    if (req.path.startWith('/users')) return 'http://localhost:4001'
-    else if (req.path.startWith('/equipament')) return 'http://localhost:4002'
-    else return 'http://localhost:4003'
-}
-
-app.use(cors())
-app.use((req,res,next)=> {
-    function httpProxy(selectProxyHost(req)(req,res,next))
+app.get('/users', function (req, res) {
+  res.json({ message: 'porta users' }).status(200)
 })
-
-app.use((req,res,next)=>{
-    next()
+app.get('/equipament', function (req, res) {
+  res.json({ message: 'porta equipament' }).status(200)
 })
-
-app.use('/users', proxy('http://localhost:4001'))
-app.use('/equipament', proxy('http://localhost:4002'))
-app.use('/', proxy('http://localhost:4003'))
-
-app.listen(port, function () {
-    console.log(`porta ${port}`)
+app.get('/', (req, res) => {
+  res.json({ message: 'porta Principal' }).status(200)
 })
-
+app.listen(4001, () => {
+  console.log(`porta 4001`)
+})
