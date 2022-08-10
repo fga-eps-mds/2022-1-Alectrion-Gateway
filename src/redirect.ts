1cb394afa9853a5471f20a5f19973a4e7ff324f5
gleal17 <guilhermelml@gmail.com>
2022-08-10 19:54:45 -0300

    #91 removendo console.log

--- src/redirect.ts
+++ src/redirect.ts
@@ -1,6 +1,5 @@
 export function selectProxyHost(path: string) {
   if (path.match('/users')) {
-    console.log('ok=>/users')
     return process.env.USER_URL
   } else if (path.match('/equipaments')) {
     return process.env.EQUIP_URL
