f568e23ff4cd677981e1f6727ea8aa3644971ed8
Moacir_MSJ <moacir.mascarenhas@vamosparcelar.com.br>
2022-08-18 01:05:59 -0300

    refactor: atualiza redirecionamento

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,23 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import httpProxy from 'express-http-proxy'
-import morgan from 'morgan'
-import { selectProxyHost } from './redirect'
+import { getHost, getUrComplement } from './redirect'
+
+dotenv.config()
 const app = express()
 
 app.use(cors())
-app.use(morgan('dev'))
 
-dotenv.config()
-
-app.use((req, res, next) => {
-  httpProxy(selectProxyHost(req.path))(req, res, next)
-  next()
-})
+app.use(
+  '/*',
+  httpProxy(getHost, {
+    proxyReqPathResolver: function (req) {
+      return getUrComplement(req)
+    }
+  })
+)
 const port = Number(process.env.PORT) || 4000
-const host = process.env.HOST || 'localhost'
 
-app.listen(port, host, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`rodando porta ${port}`)
 })
--- src/redirect.ts
+++ src/redirect.ts
@@ -1,9 +1,20 @@
-export function selectProxyHost(path: string) {
-  if (path.match('/users')) {
-    return process.env.USER_URL
-  } else if (path.match('/equipaments')) {
-    return process.env.EQUIP_URL
-  } else {
-    return process.env.BASE_URL
-  }
+import { Request } from 'express'
+
+const USER_URL = process.env.USER_URL || 'http://localhost:4001'
+const EQUIP_URL = process.env.EQUIP_URL || 'http://localhost:4002'
+
+export const getHost = (req: Request) => {
+  if (req.baseUrl.match('/user')) {
+    return USER_URL
+  } else if (req.baseUrl.match('/equipment')) {
+    return EQUIP_URL
+  } else return ''
+}
+
+export const getUrComplement = (req: Request): string => {
+  const parts = req.url.split('?')
+  const baseUrl = req.baseUrl
+  const queryString = parts[1]
+  const updatedPath = parts[0]
+  return baseUrl + updatedPath + (queryString ? '?' + queryString : '')
 }
